a9595d6dae0521503ccfce523b9572d90572304b
Mackenzie Fisher <mac@mediotype.com>
2016-12-24 14:18:58 -0700

    Resolved issues in Helper/Data.php

--- app/code/community/Magentostudy/News/Helper/Data.php
+++ app/code/community/Magentostudy/News/Helper/Data.php
@@ -1,49 +1,84 @@
 <?php
-class Magentostudy_News_Helper_Data extends Mage_Core_Helper_Data {
+/**
+ * News Data helper
+ *
+ * @author Magento
+ */
+class Magentostudy_News_Helper_Data extends Mage_Core_Helper_Data
+{
+    /**
+     * Path to store config if front-end output is enabled
+     *
+     * @var string
+     */
+    const XML_PATH_ENABLED            = 'news/view/enabled';
 
-    // path to store config if frontend output is enabled
-    /* @var string */
-    const XML_PATH_ENABLED = 'news/view/enabled';
+    /**
+     * Path to store config where count of news posts per page is stored
+     *
+     * @var string
+     */
+    const XML_PATH_ITEMS_PER_PAGE     = 'news/view/items_per_page';
 
-    // path to store config where count of news posts per page is stored
-    /* @var string */
-    const XML_PATH_ITEMS_PER_PAGE = 'news/views/items_per_page';
+    /**
+     * Path to store config where count of days while news is still recently added is stored
+     *
+     * @var string
+     */
+    const XML_PATH_DAYS_DIFFERENCE    = 'news/view/days_difference';
 
-    // path to store config where count of days while news is still recently
-    // added is stored
-    /* @var string */
-    const XML_PATH_DAYS_DIFFERENCE = 'news/view/days_difference';
+    /**
+     * News Item instance for lazy loading
+     *
+     * @var Magentostudy_News_Model_News
+     */
+    protected $_newsItemInstance;
 
-    // checks whether news can be displayed in frontend
-    /* @param integer|string|Mage_Core_Model_Store $store */
-    /* @return boolean */
-    public function isEnabled($store = null) {
+    /**
+     * Checks whether news can be displayed in the frontend
+     *
+     * @param integer|string|Mage_Core_Model_Store $store
+     * @return boolean
+     */
+    public function isEnabled($store = null)
+    {
         return Mage::getStoreConfigFlag(self::XML_PATH_ENABLED, $store);
     }
 
-    // return the number of items per page
-    /* @param integer|string|Mage_Core_Model_Store $store */
-    /* @return int */
-    public  function getNewsPerPage($store = null) {
+    /**
+     * Return the number of items per page
+     *
+     * @param integer|string|Mage_Core_Model_Store $store
+     * @return int
+     */
+    public function getNewsPerPage($store = null)
+    {
         return abs((int)Mage::getStoreConfig(self::XML_PATH_ITEMS_PER_PAGE, $store));
     }
 
-    // return difference in days while news is recently added
-    /* @param integer|string|Mage_Core_Model_Store $store */
-    /* @return int */
-    public function getDaysDifference($store = null) {
+    /**
+     * Return difference in days while news is recently added
+     *
+     * @param integer|string|Mage_Core_Model_Store $store
+     * @return int
+     */
+    public function getDaysDifference($store = null)
+    {
         return abs((int)Mage::getStoreConfig(self::XML_PATH_DAYS_DIFFERENCE, $store));
     }
 
-    // return current news item instance from the Registry
-    /* @return Magentostudy_News_Model_News */
-    public function getNewsItemInstance() {
-        if ($this->_newsItemInstance) {
+    /**
+     * Return current news item instance from the Registry
+     *
+     * @return Magentostudy_News_Model_News
+     */
+    public function getNewsItemInstance()
+    {
+        if (!$this->_newsItemInstance) {
             $this->_newsItemInstance = Mage::registry('news_item');
 
             if (!$this->_newsItemInstance) {
-                Mage::throwException($this->__('News item instance does not exist
-                in Registry'));
+                Mage::throwException($this->__('News item instance does not exist in Registry'));
             }
         }
 
